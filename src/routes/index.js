6d0cde9cb4db629bf9075f17f2f58f493124e029
DaniPoot <danielpoot.uc2@gmail.com>
2022-04-05 08:53:55 -0500

    Add the difficulties routes to the main router file

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,8 +2,10 @@ const { Router } = require('express')
 const router = Router()
 const loginRoutes = require('./login.routes.js')
 const answersRoutes = require('./answers.routes.js')
+const difficultiesRoutes = require('./difficulties.routes.js')
 
 router.use('/', loginRoutes)
 router.use('/answers', answersRoutes)
+router.use('/difficulties', difficultiesRoutes)
 
 module.exports = router
