546be92ef9b125d175d4aa19bc65ea5874cb6f23
DaniPoot <danielpoot.uc2@gmail.com>
2022-04-05 07:41:49 -0500

    Fix required method

--- src/validations/answer.js
+++ src/validations/answer.js
@@ -1,6 +1,6 @@
 const Joi = require('joi')
 
 module.exports = Joi.object({
-  description: Joi.string().require().trim(),
+  description: Joi.string().required().trim(),
   created_by: Joi.number().required().greater(0)
 })
--- src/validations/user.js
+++ src/validations/user.js
@@ -5,6 +5,6 @@ module.exports = Joi.object({
   user_name: Joi.string().required().trim().max(20),
   password: Joi.string().required().min(6).max(40),
   email: Joi.string().required().email(),
-  type: Joi.string().lowercase().require().trim(),
+  type: Joi.string().lowercase().required().trim(),
   created_by: Joi.number().greater(0)
 })
