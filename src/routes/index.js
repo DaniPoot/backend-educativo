7f793624fb57ad9d8fdb9379d9909befdcf452c8
DaniPoot <danielpoot.uc2@gmail.com>
2022-04-05 08:36:05 -0500

    Add the answers routes to the main router file

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,9 @@
 const { Router } = require('express')
 const router = Router()
 const loginRoutes = require('./login.routes.js')
+const answersRoutes = require('./answers.routes.js')
 
 router.use('/', loginRoutes)
+router.use('/answers', answersRoutes)
 
 module.exports = router
