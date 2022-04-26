84dc9e31fbced6b691bd5e22b51b353712f390e0
DaniPoot <danielpoot.uc2@gmail.com>
2022-04-25 23:23:25 -0500

    Add the routes for questions, topics and subjects

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,9 +3,15 @@ const router = Router()
 const loginRoutes = require('./login.routes.js')
 const answersRoutes = require('./answers.routes.js')
 const difficultiesRoutes = require('./difficulties.routes.js')
+const questionsRouters = require('./questions.routes.js')
+const topicsRouters = require('./topics.routes.js')
+const subjectsRouters = require('./subjects.routes.js')
 
 router.use('/', loginRoutes)
 router.use('/answers', answersRoutes)
 router.use('/difficulties', difficultiesRoutes)
+router.use('/questions', questionsRouters)
+router.use('/topics', topicsRouters)
+router.use('/subjects', subjectsRouters)
 
 module.exports = router
