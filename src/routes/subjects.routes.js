0608d034ffc569c8d2a06ba08689b63f65dac55f
DaniPoot <danielpoot.uc2@gmail.com>
2022-04-25 23:23:45 -0500

    Fix the delete method

--- src/routes/subjects.routes.js
+++ src/routes/subjects.routes.js
@@ -6,6 +6,6 @@ router.get('/', getAllSubjectsByUser)
 router.post('/create', createSubject)
 router.get('/:id', getSubject)
 router.put('/:id', updateSubject)
-router.deleted('/:id', deleteSubject)
+router.delete('/:id', deleteSubject)
 
 module.exports = router
