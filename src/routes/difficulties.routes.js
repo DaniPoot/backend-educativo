94ded257121a217371ecaf350182b0c8ab9df28b
DaniPoot <danielpoot.uc2@gmail.com>
2022-05-02 20:01:39 -0500

    Add a new route to update and  delete difficulties

--- src/routes/difficulties.routes.js
+++ src/routes/difficulties.routes.js
@@ -1,9 +1,11 @@
 const { Router } = require('express')
 const router = Router()
 
-const { createDifficulty, getDifficultiesByUser } = require('../controllers/difficulties.controller.js')
+const { createDifficulty, getDifficultiesByUser, updateDifficulty, deleteDifficulty } = require('../controllers/difficulties.controller.js')
 
 router.post('/create', createDifficulty)
 router.get('/', getDifficultiesByUser)
+router.put('/', updateDifficulty)
+router.delete('/', deleteDifficulty)
 
 module.exports = router
