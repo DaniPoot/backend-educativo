ef5961bcde4249becd36b1ceb2b2d20fc1369403
DaniPoot <danielpoot.uc2@gmail.com>
2022-07-27 19:25:33 -0500

    Add a new route to get question by topics and difficulties

--- src/controllers/questions.controller.js
+++ src/controllers/questions.controller.js
@@ -1,5 +1,5 @@
-const { Questions } = require('../database')
-const { questionValidations } = require('../validations')
+const { Questions, sequelize } = require('../database')
+const { questionValidations, questionsByTopicsDifficulties } = require('../validations')
 
 const createQuestionByUser = async (req, res) => {
   try {
@@ -76,9 +76,70 @@ const deleteQuestion = async (req, res) => {
   }
 }
 
+const getQuestionByTopicsAndDifficulties = async (req, res) => {
+  try {
+    const { body } = req
+    await questionsByTopicsDifficulties.validateAsync(body)
+    const { topics, difficulties } = body
+
+    const questions = await Questions.findAll({
+      where: {
+        id_topic: topics,
+        id_difficulty: difficulties,
+        is_deleted: false
+      },
+      limit: 10,
+      attributes: ['id', 'description', 'id_topic', 'id_difficulty'],
+      order: sequelize.random()
+    })
+
+    const newQuestions = []
+    for (const question of questions) {
+      const answersFail = await question.getAnswers({
+        limit: 1,
+        attributes: ['id', 'description'],
+        through: {
+          where: {
+            is_deleted: false,
+            is_correct: false
+          }
+        },
+        joinTableAttributes: []
+      })
+      const answersCorrect = await question.getAnswers({
+        limit: 1,
+        attributes: ['id', 'description'],
+        through: {
+          where: {
+            is_deleted: false,
+            is_correct: true
+          }
+        },
+        joinTableAttributes: []
+      })
+      newQuestions.push({
+        ...question.dataValues,
+        answers: [...answersFail, ...answersCorrect]
+      })
+    }
+
+    return res.status(201).json({
+      status: 201,
+      questions: [ newQuestions ]
+    })
+  } catch (e) {
+    const error = e.errors ? e.errors[0].message : e.message
+    return res.status(500).json({
+      status: 500,
+      error
+    })
+  }
+}
+
 module.exports = {
   createQuestionByUser,
   getAllQuestionByUser,
   updateQuestion,
-  deleteQuestion
+  deleteQuestion,
+  getQuestionByTopicsAndDifficulties
 }
--- src/routes/questions.routes.js
+++ src/routes/questions.routes.js
@@ -1,10 +1,11 @@
 const { Router } = require('express')
 const router = Router()
 
-const { createQuestionByUser, getAllQuestionByUser, updateQuestion, deleteQuestion } = require('../controllers/questions.controller.js')
+const { createQuestionByUser, getAllQuestionByUser, updateQuestion, deleteQuestion, getQuestionByTopicsAndDifficulties } = require('../controllers/questions.controller.js')
 
 router.post('/create', createQuestionByUser)
 router.get('/', getAllQuestionByUser)
+router.get('/random', getQuestionByTopicsAndDifficulties)
 router.put('/:id', updateQuestion)
 router.delete('/:id', deleteQuestion)
 
