7bc6edfd2852defd1318462025b8c720b77bd4ed
DaniPoot <danielpoot.uc2@gmail.com>
2023-04-18 07:55:35 -0600

    🚀 feat(answers.routes.js): add endpoint to update answer relation to a question

--- src/routes/answers.routes.js
+++ src/routes/answers.routes.js
@@ -1,7 +1,7 @@
 const { Router } = require('express')
 const router = Router()
 
-const { getAllAnswersByUser, createAnswerByUser, updateAnswerById, verifyAnswer, getAnswersByQuestionId } = require('../controllers/answers.controller.js')
+const { getAllAnswersByUser, createAnswerByUser, updateAnswerById, verifyAnswer, getAnswersByQuestionId, updateAnswerRelation } = require('../controllers/answers.controller.js')
 
 router.post('/create', createAnswerByUser)
 router.get('/', getAllAnswersByUser)
@@ -9,5 +9,6 @@ router.put('/:id', updateAnswerById)
 router.delete('/:id', updateAnswerById)
 router.post('/verify', verifyAnswer)
 router.get('/question/:id', getAnswersByQuestionId)
+router.post('/relation/:id', updateAnswerRelation)
 
 module.exports = router
